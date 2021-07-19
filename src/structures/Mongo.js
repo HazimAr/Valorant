126af2faa44c2a6fa58272725f1ecbca6629027e
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-19 13:27:51 -0700

    Update Mongo.js

--- src/structures/Mongo.js
+++ src/structures/Mongo.js
@@ -1,22 +1,19 @@
-const { mongoUsername, mongoPassword } = process.env,
+const { mongoUrl } = process.env,
   { connect, connection } = require("mongoose");
 module.exports = class Mongo {
   constructor(client) {
     this.client = client;
 
-    connect(
-      `mongodb+srv://${mongoUsername}:${mongoPassword}@valorant.ymqww.mongodb.net/Valorant?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        autoIndex: false,
-        poolSize: 5,
-        connectTimeoutMS: 10000,
-        family: 4,
-        useCreateIndex: false,
-        useFindAndModify: false,
-      }
-    );
+    connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: false,
+      poolSize: 5,
+      connectTimeoutMS: 10000,
+      family: 4,
+      useCreateIndex: false,
+      useFindAndModify: false,
+    });
     //
     connection.on("connected", () => console.log("MongoDB connected"));
     connection.on("disconnected", () =>
