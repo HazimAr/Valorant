a9ace08688c191e59152a8e20b1e689b1083e5fa
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-19 01:31:01 -0700

    Commando

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-const config = require("./config.json");
-const Commando = require("discord.js-commando");
+require("dotenv").config();
+const { mongoUsername, mongoPassword, token } = process.env;
+
+const { MongoClient } = require("mongodb");
+const MongoDBProvider = require("commando-provider-mongo");
 const path = require("path");
+const Commando = require("discord.js-commando");
+
+const config = require("./config.json");
 
 const client = new Commando.CommandoClient({
   owner: "682715516456140838",
-  commandPrefix: config.prefix,
 });
+
+client
+  .setProvider(
+    MongoClient.connect(
+      `mongodb+srv://${mongoUsername}:${mongoPassword}@valorant.ymqww.mongodb.net/Valorant?retryWrites=true&w=majority`
+    ).then((client) => new MongoDBProvider(client, "Valorant"))
+  )
+  .catch((err) => console.error(err));
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -16,8 +29,8 @@ client.once("ready", () => {
       ["moderation", "Moderation commands"],
       ["custom game", "Custom Game commands"],
     ])
-    .registerCommandsIn(path.join(__dirname, "commands"))
-    .registerDefaults();
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, "commands"));
 });
 
-client.login(config.token);
+client.login(token);
